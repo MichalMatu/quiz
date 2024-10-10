a8686e0514b4c1856ac03313016a4e878478f1b4
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-10 14:58:06 +0200

    first steps

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.red,
+                Colors.green,
+                Colors.blue,
+              ],
+            ),
+          ),
+          // add image on top of the gradient
+          child: Stack(
+            children: [
+              Image.asset('assets/images/quiz-logo.png'),
+              Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: AppBar(
+                  // background color transparent
+                  backgroundColor: Colors.transparent,
+                ),
+                body: const Center(
+                  child: Text('Hello World'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

f7dc42d63004447a1365358bfa46508bca54c542
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-10 15:38:35 +0200

    frame ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// import material.dart file
 import 'package:flutter/material.dart';
 
 // main function
@@ -8,9 +7,12 @@ void main() {
 }
 
 // MyApp class
-
 class MyApp extends StatelessWidget {
   const MyApp(Key? key) : super(key: key);
+
+  void startQuiz() {
+    print('Quiz started');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,23 +26,42 @@ class MyApp extends StatelessWidget {
                 Colors.green,
                 Colors.blue,
               ],
+              begin: Alignment.topLeft, // Start from the top-left
+              end: Alignment.bottomRight, // End at the bottom-right
             ),
           ),
-          // add image on top of the gradient
-          child: Stack(
-            children: [
-              Image.asset('assets/images/quiz-logo.png'),
-              Scaffold(
-                backgroundColor: Colors.transparent,
-                appBar: AppBar(
-                  // background color transparent
-                  backgroundColor: Colors.transparent,
+          // Column to stack image and text vertically in the center
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Center the column
+              children: [
+                // Image displayed above the text
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  fit: BoxFit.cover, // Ensure the image covers the area
+                  width: 200, // Set a specific width if needed
+                  height: 200, // Set a specific height if needed
                 ),
-                body: const Center(
-                  child: Text('Hello World'),
+                const SizedBox(height: 24), // Spacer between image and text
+                const Text(
+                  'Let\'s Quiz',
+                  style: TextStyle(
+                    fontSize: 32, // Set your desired font size
+                    color: Colors.white, // Set the text color
+                    fontWeight: FontWeight.bold, // Make it bold
+                  ),
                 ),
-              ),
-            ],
+                // Spacer between text and button
+                const SizedBox(height: 24),
+                // add button qith test StartQuiz in simplest form
+                ElevatedButton(
+                  onPressed: () {
+                    startQuiz();
+                  },
+                  child: const Text('Start Quiz'),
+                ),
+              ],
+            ),
           ),
         ),
       ),

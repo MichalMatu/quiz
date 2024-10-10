644ce7ec3ed3fd55c3f6ce01dfaee26fd2cab0f2
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-10 15:57:07 +0200

    other way

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,14 @@
 import 'package:flutter/material.dart';
+import 'start_screen.dart';
 
 // main function
 void main() {
   // runApp function
-  runApp(const MyApp(null));
-}
-
-// MyApp class
-class MyApp extends StatelessWidget {
-  const MyApp(Key? key) : super(key: key);
-
-  void startQuiz() {
-    print('Quiz started');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.red,
-                Colors.green,
-                Colors.blue,
-              ],
-              begin: Alignment.topLeft, // Start from the top-left
-              end: Alignment.bottomRight, // End at the bottom-right
-            ),
-          ),
-          // Column to stack image and text vertically in the center
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Center the column
-              children: [
-                // Image displayed above the text
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  fit: BoxFit.cover, // Ensure the image covers the area
-                  width: 200, // Set a specific width if needed
-                  height: 200, // Set a specific height if needed
-                ),
-                const SizedBox(height: 24), // Spacer between image and text
-                const Text(
-                  'Let\'s Quiz',
-                  style: TextStyle(
-                    fontSize: 32, // Set your desired font size
-                    color: Colors.white, // Set the text color
-                    fontWeight: FontWeight.bold, // Make it bold
-                  ),
-                ),
-                // Spacer between text and button
-                const SizedBox(height: 24),
-                // add button qith test StartQuiz in simplest form
-                ElevatedButton(
-                  onPressed: () {
-                    startQuiz();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green, // Background color
-                    foregroundColor: Colors.white, // Text color
-                    elevation: 5, // Elevation
-                    shape: RoundedRectangleBorder(
-                      // Button shape
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: const Text('Start Quiz'),
-                ),
-              ],
-            ),
-          ),
-        ),
+        body: StartScreen(),
       ),
-    );
-  }
+    ),
+  );
 }

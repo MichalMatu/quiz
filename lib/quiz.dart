c5d86c8831fd5e352fffb285bf319dd3ba734a8e
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-10 21:59:01 +0200

    if statement

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -21,6 +21,11 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -34,9 +39,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomCenter,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );

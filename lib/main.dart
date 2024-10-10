241a5b1d85e4ffb491b6ec1cb2a657d3577a4bab
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-10 16:17:45 +0200

    SZABLON

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,20 @@ import 'start_screen.dart';
 void main() {
   // runApp function
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 58, 141, 208),
+                  Color.fromARGB(255, 72, 93, 231),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: const StartScreen()),
       ),
     ),
   );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,6 +4,30 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Text('Hello World');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // add image here
+          Image.asset('assets/images/quiz-logo.png', width: 200, height: 200),
+          const SizedBox(height: 20),
+          const Text(
+            'Quiz App',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          // use play button here
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.blue,
+            child: const Icon(Icons.play_arrow),
+          ),
+        ],
+      ),
+    );
   }
 }

b6f86010afe3e23d357ab70a154acf27a3d4bf6c
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-12 15:51:42 +0200

    fixed

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
 
-  final List<Map<String, Object>> summaryData;
+  final List<Map<String, Object?>>
+      summaryData; // Change Object to Object? to allow null values.
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +12,21 @@ class QuestionsSummary extends StatelessWidget {
       children: summaryData.map((data) {
         return Row(
           children: [
-            Text(((data['question_index'] as int) + 1).toString()),
+            Text(((data['question_index'] as int?) ?? 0 + 1)
+                .toString()), // Add null check
             Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Align texts to start
               children: [
-                Text(data['question'] as String),
+                Text(data['question'] as String? ??
+                    'No question'), // Add null check
                 const SizedBox(
                   height: 5,
                 ),
-                Text(data['user_answer'] as String),
-                Text(data['correct_answer'] as String),
+                Text(data['user_answer'] as String? ??
+                    'No answer'), // Add null check
+                Text(data['correct_answer'] as String? ??
+                    'No correct answer'), // Add null check
               ],
             ),
           ],
